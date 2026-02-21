820b689b501d64493df9bad0998afa478e0a7b5f
unknown <sridhars0305@gmail.com>
2026-02-21 22:24:25 +0530

    Connected frontend to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
 
     try {
       const res = await axios.post(
-        "http://127.0.0.1:8000/predict",
+        "https://ai-vqc-fruit.onrender.com/predict",
         formData
       );
 
